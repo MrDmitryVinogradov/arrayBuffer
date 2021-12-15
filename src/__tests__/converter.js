d09c303f1ec2429372e8bbd1317b4d207b21712f
Dmitry Vinogradov <mr.dmitryvinogradov@gmail.com>
2021-12-15 14:54:05 +0300

    Linter added

--- src/__tests__/converter.js
+++ src/__tests__/converter.js
@@ -1,11 +1,11 @@
 import getBuffer from '../js/functions/getBuffer';
 
-import ArrayBufferConverter from '..//js/classes/ArrayBufferConverter'
+import ArrayBufferConverter from '../js/classes/ArrayBufferConverter';
 
 test('should read ArrayBuffer', () => {
   const buffer = getBuffer();
-  const bufferConverter = new ArrayBufferConverter;
+  const bufferConverter = new ArrayBufferConverter();
   bufferConverter.load(buffer);
-  const result = bufferConverter.toString()
+  const result = bufferConverter.toString();
   expect(result).toBe('{"data":{"user":{"id":1,"name":"Hitman","level":10}}}');
 });
--- src/js/classes/ArrayBufferConverter.js
+++ src/js/classes/ArrayBufferConverter.js
@@ -1,15 +1,14 @@
 export default class ArrayBufferConverter {
-  constructor() {
-  }
   load(buffer) {
     this.buffer = buffer;
   }
+
   toString() {
     const bufferView = new Uint16Array(this.buffer);
     let stringView = '';
-    for (let i = 0; i < bufferView.length; i++) {
+    for (let i = 0; i < bufferView.length; i += 1) {
       stringView += String.fromCharCode(bufferView[i]);
     }
-    return stringView
+    return stringView;
   }
 }
